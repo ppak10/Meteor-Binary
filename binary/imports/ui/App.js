d36577d10a5c517f8dfa2325fc388077ccf59fe4
ppak10 <37542469+ppak10@users.noreply.github.com>
2018-09-23 13:20:58 -0400

    Publish and Subscribe
    Commit adds functionality to selectively publish and subscribe as to add further security

--- binary/imports/ui/App.js
+++ binary/imports/ui/App.js
@@ -53,9 +53,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   // Render Component ---------------------------------------------------------
@@ -100,6 +109,8 @@ class App extends Component {
 
 // Export Component -----------------------------------------------------------
 export default withTracker(() => {
+  Meteor.subscribe('tasks');
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
